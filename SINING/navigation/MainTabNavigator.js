cdb5f09e97e8ce12adce7d9200a33279bc0b9b59
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2018-12-15 17:25:19 +0800

    Simple Classifier GUI done (non-dynamic)

--- SINING/navigation/MainTabNavigator.js
+++ SINING/navigation/MainTabNavigator.js
@@ -4,7 +4,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+import ClassifiedScreen from '../screens/ClassifiedScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
@@ -25,12 +25,12 @@ HomeStack.navigationOptions = {
   ),
 };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+const ClassifiedStack = createStackNavigator({
+  Classified: ClassifiedScreen,
 });
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+ClassifiedStack.navigationOptions = {
+  tabBarLabel: 'Classified',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
@@ -55,6 +55,6 @@ SettingsStack.navigationOptions = {
 
 export default createBottomTabNavigator({
   HomeStack,
-  LinksStack,
+  ClassifiedStack,
   SettingsStack,
 });
