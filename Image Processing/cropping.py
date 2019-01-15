fe208efa9f6648d5e6186b4614ae6905c9e4e36f
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2019-01-15 17:30:42 +0800

    Sectioned into 9 overlapping segments

--- Image Processing/cropping.py
+++ Image Processing/cropping.py
@@ -73,14 +73,37 @@ def cut4Sections(painting):
 	width, height = painting.shape[:2]
 	middleW = round(width/2)
 	middleH = round(height/2)
+	middleWW =  round(middleW/2)
+	middleHH =  round(middleH/2)
+
 	topL = painting[0:middleW,0:middleH]
+	middleL = painting[middleWW:middleW+middleWW,0:middleH]
 	bottomL = painting[middleW:width,0:middleH]
+	
+	topM = painting[0:middleW,middleHH:middleH+middleHH]
+	middleM = painting[middleWW:middleW+middleWW,middleHH:middleH+middleHH]
+	bottomM = painting[middleW:width,middleHH:middleH+middleHH]
+
 	topR = painting[0:middleW,middleH:height]
+	middleR = painting[middleWW:middleW+middleWW,middleH:height]
 	bottomR = painting[middleW:width,middleH:height]
-	# cv2.imwrite("topL.jpg", topL)
-	# cv2.imwrite("topR.jpg", topR)
+
+	cv2.imwrite("topL.jpg", topL)
+	cv2.imwrite("middleL.jpg", middleL)
+	cv2.imwrite("bottomL.jpg", bottomL)
+
+	cv2.imwrite("topM.jpg", topM)
+	cv2.imwrite("middleM.jpg", middleM)
+	cv2.imwrite("bottomM.jpg", bottomM)
+	
+	cv2.imwrite("topR.jpg", topR)
+	cv2.imwrite("middleR.jpg", middleR)
+	cv2.imwrite("bottomR.jpg", bottomR)
+
 	# cv2.imwrite("bottomL.jpg", bottomL)
 	# cv2.imwrite("bottomR.jpg", bottomR)
+
+
 
 def rotate(image, degree):
 	num_rows, num_cols = image.shape[:2]
@@ -89,7 +112,7 @@ def rotate(image, degree):
 	rotation = cv2.warpAffine(image, rotation_matrix, (num_cols, num_rows))
 	return rotation
 
-painting = cropImage("try.jpg")
+painting = cropImage("heightTry.jpg")
 # square = cropSquareImage(painting)
 cut4Sections(painting)
 # square = cv2.resize(square, (250, 250)) 
