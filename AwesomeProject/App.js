fd65cf08a667fae065d562dc98f0dd2345936faa
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2019-01-16 14:45:17 +0800

    Picking of image and camera works perfectly (w/o square view)

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,168 +1,71 @@
-import Camera from 'react-native-camera';
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Image,
-  ScrollView,
   StyleSheet,
   Text,
   View,
-  CameraRoll,
-  PermissionsAndroid
+  Image,
+  Button
 } from 'react-native';
+import ImagePicker from "react-native-image-picker";
 
-export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+
+export default class App extends Component {
+
   state = {
-    photos: []
+    pickedImage: null
   }
+
+  reset = () => {
+    this.setState({
+      pickedImage: null
+    });
+  }
+
+  pickImageHandler = () => {
+    ImagePicker.showImagePicker({ title: "Pick a Painting", maxWidth: 800, maxHeight: 600 }, res => {
+      if (res.didCancel) {
+        console.log("User cancelled!");
+      } else if (res.error) {
+        console.log("Error", res.error);
+      } else {
+        console.log('heyyyy')
+        this.setState({
+          pickedImage: { uri: res.uri }
+        });
+        console.log(this.state.pickedImage)
+
+      }
+    });
+  }
+
+  resetHandler = () => {
+    this.reset();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>
-              Sining!
-            </Text>
-          </View>
+        <View style={styles.getStartedContainer}>
+          <Text style={styles.getStartedText}>Sining </Text>
+        </View>
+        <View style={styles.placeholder}>
+          <Image source={this.state.pickedImage} style={styles.previewImage} />
+        </View>
+        <View style={styles.button}>
 
-          <View style={styles.getStartedContainerButtons}>
-            <Button
-              buttonStyle={{
-                backgroundColor: "rgba(92, 99,216, 1)",
-                width: 250,
-                height: 45,
-                borderColor: "transparent",
-                borderWidth: 0,
-                borderRadius: 5
-              }}
-              containerStyle={{ marginTop: 30 }}
-              icon={
-                <Icon
-                  name='arrow-right'
-                  size={15}
-                  color='white'
-                />
-              }
-              onPress={this.takePicture}
+          <Button title="Pick Image" onPress={this.pickImageHandler} />
 
-              title='Camera'
-            /
-            >
-            <Camera
-              ref={(cam) => {
-                this.camera = cam;
-              }}
-              style={styles.preview}>
-              <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-            </Camera>
-            <Button
-              buttonStyle={{
-                backgroundColor: "rgba(92, 99,216, 1)",
-                width: 250,
-                height: 45,
-                borderColor: "transparent",
-                borderWidth: 0,
-                borderRadius: 5
-              }}
-              containerStyle={{ marginTop: 10 }}
-              icon={
-                <Icon
-                  name='arrow-right'
-                  size={15}
-                  color='white'
-                />
-              }
-              onPress={this._handleButtonPress}
-              title='Gallery'
-            />
-            <ScrollView>
-              {this.state.photos.map((p, i) => {
-                return (
-                  <Image
-                    key={i}
-                    style={{
-                      width: 300,
-                      height: 100,
-                    }}
-                    source={{ uri: p.node.image.uri }}
-                  />
-                );
-              })}
-            </ScrollView>
+          <Button title="Reset" onPress={this.resetHandler} />
 
-          </View>
-
-        </ScrollView>
-
+        </View>
       </View>
     );
   }
-  takePicture() {
-    const options = {};
-    //options.location = ...
-    this.camera.capture({ metadata: options })
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
-  }
-  _handleButtonPress = async () => {
-    console.log('hey')
-    try {
-      console.log('heyy')
-
-      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-        {
-          'title': 'Photos Permission',
-          'message': 'Cool Photo App needs access to your camera so you can take awesome pictures.'
-        }
-      )
-
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        alert("You can use the camera")
-        CameraRoll.getPhotos({
-          first: 20,
-          assetType: 'Photos',
-        })
-          .then(r => {
-            this.setState({ photos: r.edges });
-            console.log('somethin')
-          })
-          .catch((err) => {
-            console.log("noop")
-            console.log(err)
-            //Error Loading Images
-          });
-      } else {
-        alert("Camera permission denied")
-      }
-    } catch (err) {
-      console.warn(err)
-    }
-
-
-
-
-  };
-
-
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-    marginTop: 150,
-    backgroundColor: '#fff',
-  },
-  getStartedContainerButtons: {
-    alignItems: 'center',
+    alignItems: "center"
   },
   getStartedText: {
     fontSize: 50,
@@ -170,17 +73,34 @@ const styles = StyleSheet.create({
     lineHeight: 50,
     textAlign: 'center',
   },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+    marginTop: 80,
   },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
-  }
-});
+  textStyle: {
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "center",
+    color: "red",
+    marginTop: 10
+  },
+  placeholder: {
+    backgroundColor: "#eee",
+    width: 250,
+    height: 250,
+    marginTop: 30,
+  },
+  button: {
+    width: "80%",
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-around"
+  },
+  previewImage: {
+    width: "100%",
+    height: "100%"
+  },
+
+
+})
